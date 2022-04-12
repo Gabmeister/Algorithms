8021f40bf47e1bba0f89d140c06570c74b06e461
Gabmeister <mememailjkrofl10@gmail.com>
2022-04-12 14:35:01 +0100

    kruskals description, not yet coded.

--- Main/src/Algo/Kruskals.java
+++ Main/src/Algo/Kruskals.java
@@ -1,7 +1,23 @@
 package Algo;
+import java.util.*;  
 
-public class Kruskals {
-    public void edge(){
-        
+/*
+ALGORITHM DESCRIPTION -- TO BE CODED 
+1)Create set E which includes all edges
+2)Soft set E in ascending order of weight
+3)Iterate through set E and check if adding the element creates a cycle
+4)If yes: Discard element
+5)If no: Add to spanning tree and move on to next element
+6)End algorithm once all vertices are reached (E = (Vertices - 1))
+*/
+class Kruskals {
+    class Edge implements Comparable<Edge>{
+        int src, dest, weight;
+
+        public int compareTo(Edge compareEdge){
+        return this.weight - compareEdge.weight;
+        }
     }
+
+    
 }
