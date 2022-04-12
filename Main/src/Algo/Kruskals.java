0b951639c80422f79a1236704d9741ad51ce7395
Gabmeister <mememailjkrofl10@gmail.com>
2022-04-12 16:09:49 +0100

    desc update

--- Main/src/Algo/Kruskals.java
+++ Main/src/Algo/Kruskals.java
@@ -7,17 +7,12 @@ ALGORITHM DESCRIPTION -- TO BE CODED
 2)Soft set E in ascending order of weight
 3)Iterate through set E and check if adding the element creates a cycle
 4)If yes: Discard element
-5)If no: Add to spanning tree and move on to next element
-6)End algorithm once all vertices are reached (E = (Vertices - 1))
+5)If no: Add to spanning tree via Union() and move on to next element
+5i)Union() is used to add an edge/element into the minimum spanning tree.
+6)End algorithm once all vertices are reached (E = (Vertices - 1)) as MST is found
+7)Return MST & Elements
 */
 class Kruskals {
-    class Edge implements Comparable<Edge>{
-        int src, dest, weight;
-
-        public int compareTo(Edge compareEdge){
-        return this.weight - compareEdge.weight;
-        }
-    }
 
     
 }
